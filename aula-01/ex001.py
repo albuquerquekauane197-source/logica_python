51683c769cf0eeab26b59a060e27e13cafe3acdb
albuquerquekauane197-source <albuquerquekauane197@gmail.com>
2026-06-19 10:59:52 -0300

    Feat(ex001): adicionar o exercício 01

---
+++ aula-01/ex001.py
@@ -0,0 +1,8 @@
+nome = "Ana lima"
+idade = 28
+peso = 62.5
+print(f"ola, meu nome é {nome}, tenho {idade} anos e peso {peso} kg.")
+nome = input("qual é seu nome? ")
+idade = input("quantos anos voce tem? ")
+peso = input("quanto voce pesa? ")
+print(f"ola {nome}, voce tem {idade} anos e pesa {pesa} kg. ")
