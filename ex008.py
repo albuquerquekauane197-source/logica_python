2b26aaf0c3933d77d9c78a141c7d9ed496c6f5a0
albuquerquekauane197-source <albuquerquekauane197@gmail.com>
2026-06-23 11:11:52 -0300

    feat(ex008):adicionar exercicio 08

---
+++ ex008.py
@@ -0,0 +1,9 @@
+medida = float(input("Distancia em metros: "))
+print(f" quilômetros: {medida / 1000:.5f} km")
+print(f" Hectômetros: {medida / 100:.4f} hm")
+print(f" Decâmetros: {medida / 10:.3f} dam")
+print(f" Decímetros: {medida * 10:.1f} dm")
+print(f" milímetros: {medida *100:.1f} cm")
+print(f" milímetros: {medida * 1000:.1f} mm")
+
+ 
