3923804525de486480e5f61063a80dc208913e8b
albuquerquekauane197-source <albuquerquekauane197@gmail.com>
2026-06-23 09:13:15 -0300

    upload kakau.py

---
+++ aula-01/kakau.py
@@ -0,0 +1,15 @@
+n1 = int(input("digite um numero: "))
+
+soma2 = n1 * 2
+soma3 = n1 * 3
+soma4 = n1 * 4
+soma5 = n1 * 5
+
+print(soma2)  
+print(soma3)  
+print(soma4)  
+print(soma5)  
+
+
+
+    
---
+++ aula-01/somatorio.py
@@ -0,0 +1,10 @@
+def soma(n1,n2):
+    resultado = n1 + n2
+    return resultado
+def principal():
+
+    numero1 = int (input("digite n1: "))
+    numero2 = int(input("digite n2 "))
+    resultado = soma(numero1, numero2)
+    print ("somatorio é:" , resultado)
+principal()
